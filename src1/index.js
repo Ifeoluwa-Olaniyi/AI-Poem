3ca3c1f95a4d15beb4cb01ba187c46eaf1ddfeb9
Ifeoluwa <olaniyiife896@gmail.com>
2025-03-03 12:43:35 +0200

    Index

--- src1/index.js
+++ src1/index.js
@@ -1,1 +1,15 @@
-alert("Hello World");
+function generatePoem(event){
+    event.preventDefault();
+
+
+new Typewriter('#poem', {
+  strings: "Words to My Love",
+  autoStart: true,
+  delay: 1,
+  cursor: ""
+});
+PoemElement.innerHTML= "Words to My Love";
+}
+
+let poemFormElement= document.querySelector("#poem-generator-form")
+poemFormElement.addEventListener("submit", generatePoem);
