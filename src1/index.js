fb33881deb1aa3773f0ab43fa6faa4220c23ac69
Ifeoluwa <olaniyiife896@gmail.com>
2025-03-05 13:15:53 +0200

    New

--- src1/index.js
+++ src1/index.js
@@ -1,14 +1,30 @@
-function generatePoem(event){
-    event.preventDefault();
-
-
+function displayPoem(response){
+  console.log("poem generatated");
 new Typewriter('#poem', {
-  strings: "Words to My Love",
+  strings: response.data.answer,
   autoStart: true,
   delay: 1,
   cursor: ""
-});
-PoemElement.innerHTML= "Words to My Love";
+})
+}
+
+function generatePoem(event){
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instruction");
+let apiKey = "f703e5a61eta6cab06o3b9dd14f7dc6e";
+let context ="You are a romantic Poem expert and love to write short poems. Your mission is to generate 4 line poem in ba0sic HTML and separate each line. Make sure you follow the user instructions.Do not include a title to the poem. Sign the poem with 'SheCodes ai' inside a <strong> element at the end of the poem and NOT at the beginning.";
+let prompt ='User instructions: Generate a poem about flowers" ${instructionsInput.value}';
+let apiURL = "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}";
+
+console.log("Generating poem");
+console.log('Prompt:${prompt}');
+console.log('Context:${context}');
+
+axios.get(apiURL).then(displayPoem);
+
+
+;
 }
 
 let poemFormElement= document.querySelector("#poem-generator-form")
